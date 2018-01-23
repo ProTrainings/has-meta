e5141dbfea1371407c8d7cf462a19b782b5e2119
Dan Drust <dan.drust@protrainings.com>
2018-01-23 09:07:49 -0600

    Change expectations for respond_to? tests to be true or false, not truthy

--- spec/has_meta/has_meta_spec.rb
+++ spec/has_meta/has_meta_spec.rb
@@ -6,68 +6,68 @@ RSpec.describe HasMeta do
   describe '#respond_to?' do
     it 'responds to target_model getter' do
       model = MetaModel.new
-      expect(model.respond_to? :target_model).to be_truthy
+      expect(model.respond_to? :target_model).to be true
     end
 
     it 'responds to target_model setter' do
       model = MetaModel.new
-      expect(model.respond_to? :target_model=).to be_truthy
+      expect(model.respond_to? :target_model=).to be true
     end
 
     it 'doesn\'t respond to target_models getter' do
       model = MetaModel.new
-      expect(model.respond_to? :target_models).not_to be_truthy
+      expect(model.respond_to? :target_models).to be false
     end
 
     # Change automatic addtion of "_id" to look at foregin key name
     it 'responds to target_model_id getter' do
       model = MetaModel.new
-      expect(model.respond_to? :target_model_id).to be_truthy
+      expect(model.respond_to? :target_model_id).to be true
     end
 
     it 'responds to target_model_id setter' do
       model = MetaModel.new
-      expect(model.respond_to? :target_model_id=).to be_truthy
+      expect(model.respond_to? :target_model_id=).to be true
     end
 
     it 'responds to foo_id getter' do
       model = MetaModel.new
-      expect(model.respond_to? :foo_id).to be_truthy
+      expect(model.respond_to? :foo_id).to be true
     end
 
     it 'responds to foo_id setter' do
       model = MetaModel.new
-      expect(model.respond_to? :foo_id=).to be_truthy
+      expect(model.respond_to? :foo_id=).to be true
     end
 
     it 'doesn\'t respond to foo setter' do
       model = MetaModel.new
-      expect(model.respond_to? :foo=).not_to be_truthy
+      expect(model.respond_to? :foo=).to be false
     end
 
     it 'doesn\'t respond to foo getter' do
       model = MetaModel.new
-      expect(model.respond_to? :foo).not_to be_truthy
+      expect(model.respond_to? :foo).to be false
     end
 
     it 'responds to foo_bar setter' do
       model = MetaModel.new
-      expect(model.respond_to? :foo_bar=).to be_truthy
+      expect(model.respond_to? :foo_bar=).to be true
     end
 
     it 'responds to foo_bar getter' do
       model = MetaModel.new
-      expect(model.respond_to? :foo_bar).to be_truthy
+      expect(model.respond_to? :foo_bar).to be true
     end
   
     it 'doesn\'t respond to foo_bar_id getter' do
       model = MetaModel.new
-      expect(model.respond_to? :foo_bar_id).not_to be_truthy
+      expect(model.respond_to? :foo_bar_id).to be false
     end
   
     it 'doesn\'t respond to foo_bar_id setter' do
       model = MetaModel.new
-      expect(model.respond_to? :foo_bar_id=).not_to be_truthy
+      expect(model.respond_to? :foo_bar_id=).to be false
     end
   end
   
@@ -81,15 +81,15 @@ RSpec.describe HasMeta do
     end
 
     it 'responds to find_by_foo_id' do
-      expect(MetaModel.respond_to? :find_by_foo_id).to be_truthy
+      expect(MetaModel.respond_to? :find_by_foo_id).to be true
     end
 
     it 'doesn\'t respond to find_by_foo' do
-      expect(MetaModel.respond_to? :find_by_foo).not_to be_truthy
+      expect(MetaModel.respond_to? :find_by_foo).to be false
     end
 
     it 'responds to find_by_foo_bar' do
-      expect(MetaModel.respond_to? :find_by_foo_bar).to be_truthy
+      expect(MetaModel.respond_to? :find_by_foo_bar).to be true
     end
   end
   
